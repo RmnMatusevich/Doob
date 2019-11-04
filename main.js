05e4e5186d0baed49e8e801c2ba9f5610958a5c1
RmnMatusevich <rmnmatusevich@gmail.com>
2019-11-04 22:30:26 +0300

    Make mobile first
    feat: add landing for mobile divices

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+let button = document.getElementsByClassName("menu");
+let nav = document.getElementsByClassName("main_nav");
+button.onclick = () => {
+    nav.style.display = "visible";
+};
+
+function f() {
+    let nav = document.getElementsByClassName("main_nav");
+    nav.style.display = "visible";
+}
